39a1590ed3221e482c8d40eba96f8ea3d9e200cb
karthik-arjunan <karthik46.karthik@gmail.com>
2025-02-05 15:18:08 +0530

    created folder structure, empty functions pass

--- test/unit_test.py
+++
@@ -1,4 +0,0 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-    pass
---
+++ unit_test.py
@@ -0,0 +1,5 @@
+import unittest
+from main import StringCalculator
+class TestStringCalculator(unittest.TestCase):
+    def test_empty_str(self):
+        pass
