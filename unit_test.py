ce5eb01997dd06d6c1dd4450393ec2ac291d796a
karthik-arjunan <karthik46.karthik@gmail.com>
2025-02-05 15:26:30 +0530

    test number with delimiter test case pass

--- unit_test.py
+++ unit_test.py
@@ -8,3 +8,8 @@ class TestStringCalculator(unittest.TestCase):
     def test_single_number(self):
         calculator = StringCalculator()
         self.assertEqual(calculator.add("1"), 1)
+        
+    def test_number_with_delimiter(self):
+        calculator = StringCalculator()
+        self.assertEqual(calculator.add("1,2"), 3)
+        self.assertEqual(calculator.add("1,2,3"), 6)
