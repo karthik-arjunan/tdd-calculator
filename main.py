727839f72d7557943f699576ff99ec6858520f42
karthik-arjunan <karthik46.karthik@gmail.com>
2025-02-05 15:45:47 +0530

    test custom delimiter function added and text case passed

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
-        return sum(map(int, numbers.replace("\n", ",").split(",")))
+        
+        delimiter = ","
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = parts[0][2:]
+            numbers = parts[1]
+        
+        return sum(map(int, numbers.replace("\n", delimiter).split(delimiter)))
